45c107ee50f563822fb50c00b07024ee5e66a543
Naomi Kapele <naomikapele@booknowzambia.com>
2024-11-22 07:11:05 +0200

    animatiing score using javascript

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const correctAnwsers = ['B','A','A','A','B','B'];
 
 const form = document.querySelector('.quiz-form');
 
+const result = document.querySelector('.result');
+
+const userScore = document.querySelector('.result span');
+console.log(userScore);
+
+
+
 // console.log(form.q1.value, form.q2.value, form.q3.value,form.q4.value, form.q5.value, form.q6.value);
 
 
@@ -20,5 +27,30 @@ form.addEventListener('submit', e => {
     }
   });
 
-  console.log(Math.round(score));
+  
+  // show result on page
+
+  // result.classList.remove('d-none');
+  // const roundedScore = Math.round(score);
+  // userScore.textContent = `${roundedScore}%`;
+  // or
+  scrollTo(0,0);
+  const roundedScore = Math.round(score);
+  // result.querySelector('span').textContent = `${roundedScore}%`;
+  result.classList.remove('d-none');
+
+  // animating the score
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === roundedScore){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10)
+  
 });
+
+
+
